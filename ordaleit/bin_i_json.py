9e67ff28ec7f1381251d47721564c92189de9143
OliverMani <oliverhilmars@gmail.com>
2020-06-10 21:50:29 +0000

    Núna geturðu bætt við nafnorðum

--- ordaleit/bin_i_json.py
+++ ordaleit/bin_i_json.py
@@ -47,20 +47,84 @@ class Ord:
 	def __init__(self, gogn):
 		pass
 
+	def to_json(self):
+		return {}
+
 
 class MainClass:
 
-	
+	def setup(self):
+		pass
 
 	def append_data(self, data):
 		listi = {}
 		flokkur = data[0].get('ofl')
 		kyn = data[0].get('kyn')
-		
+		komid = list(self.listi.data['no'].keys())
+		if data[0]['ord'] in komid:
+			print("Orðið er nú þegar þarna!")
+			return
+
+		if flokkur == "no":
+			
+			for mynd in data[0].get('bmyndir'):
+				if listi.get('mynd') == None:
+					listi['mynd'] = {}
+				listi['kyn'] = data[0].get('kyn')
+				fall = mynd.get('g')[0:2]
+				bil = 1 if fall[1] == 'G' else 0
+				tala = mynd.get('g')[2 + bil: 4 + bil]
+				if bil == 1:
+					fall += 'F'
+				greinir = 'gr' in mynd.get('g')
+				#print("Mynd:", mynd.get('b'), "fall:", fall, "tala:", tala, "greinir:", greinir)
+
+				strgreinir = 'greinir' if greinir else 'ekki_greinir'
+
+				if listi['mynd'].get(strgreinir) == None:
+					listi['mynd'][strgreinir] = {}
+				if listi['mynd'].get(strgreinir).get(tala.lower()) == None:
+					listi['mynd'][strgreinir][tala.lower()] = {}
+
+
+				listi['mynd'][strgreinir][tala.lower()][fall.lower()] = mynd.get('b')
+
+
+				"""
+				Uppbygging:
+				{
+					'greinir':{
+						'et': {
+							föll...
+						},
+						'ft': {
+							föll...
+						}
+					}, 'ekki_greinir': {
+						'et': {
+							föll...
+						},
+						'ft': {
+							föll...
+						}
+					}
+				}
+				"""
+			#print(listi)
+			if self.listi.data.get('no') == None:
+				self.listi.data['no'] = {}
+			self.listi.data['no'][data[0].get('ord')] = listi
+			print("Orði bætt við!")
+
+		elif flokkur == "so":
+			pass
+		elif flokkur == "lo":
+			pass
 
 
 	def main(self, args):
 		self.listi = Uppsetning('resources/ord.json')
+		self.setup()
 
 		word = ""
 		if len(args) > 0:
@@ -68,9 +132,12 @@ class MainClass:
 		else:
 			word = input("Sláðu inn nafnorð: ")
 		r = requests.get("https://bin.arnastofnun.is/api/ord/" + word)
-		print(r.status_code)
+		#print(r.status_code)
 		data = json.loads(r.content)
-		print(data)
+		if type(data) != list:
+			print("Orð er ekki til!")
+			return
+			
 		if len(data) > 1:
 			for x in range(len(data)):
 				guid = data[x].get('guid')
@@ -78,7 +145,9 @@ class MainClass:
 				self.append_data(new_r.content)
 		else:
 			self.append_data(data)
-		print(len(data))
+
+		self.listi.save()
+		
 
 
 if __name__ == '__main__':
